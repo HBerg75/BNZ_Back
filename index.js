d0ccf7c110f1288e367c5e9245b1b3cf12d78f19
hberg75 <yanis.benhagouga@epitech.eu>
2023-04-07 15:02:50 +0200

    [START] Start back-end with standard model, routes, connect bdd etc ...

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDB = require('./services/db');
+const app = express();
+
+app.use(express.json());
+app.use('/api/users', require('./routes/userRoute'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('internal server error');
+});
+
+connectDB().then(() => {
+  console.log('Connected to DB');
+}).catch(err => {
+  console.error('Unable to connect to DB', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
+});
---
+++ services/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.MONGODB_URI_LOCAL;
+console.log('uri', uri);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
